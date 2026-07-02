08a2aef4f16ce2f086b92296c9175ee265bd41bc
paliwaladitya298 <paliwaladitya298@gmail.com>
2026-07-02 21:32:03 +0530

    Delete src directory

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { searchUsers } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/', protect, searchUsers);
-
-module.exports = router;
